0fdeaee9bd6eca4addfc8b36c953c731ca91f428
KIRIAKE Masanori <seiken@rd01.net>
2025-05-04 06:19:14 +0900

    Refactor notifyConnection and enhance keySend functionality with keyboard mode support

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,101 @@
 #include <BleKeyboard.h>
 #include <M5Cardputer.h>
 
+#include <map>
+
 BleKeyboard bleKey;
 
 void notifyConnection() {
   static bool connected = false;
   if (bleKey.isConnected() && !connected) {
-    Serial.println("Connected");
     connected = true;
     M5Cardputer.Speaker.tone(2000, 100);
+    delay(100);
+    M5Cardputer.Speaker.tone(2300, 100);
   } else if (!bleKey.isConnected() && connected) {
     connected = false;
-    M5Cardputer.Speaker.tone(1000, 100);
-    Serial.println("Disconnected");
+    M5Cardputer.Speaker.tone(2300, 100);
+    delay(100);
+    M5Cardputer.Speaker.tone(2000, 100);
   }
 }
+enum class KeyboardMode { normal, numeric, cursor, function, media };
 
+void charSend(char k, KeyboardMode mode) {
+  const std::map<char, char> numKeyMap = {
+      {',', '*'}, {'.', '0'}, {'/', '#'}, {'l', '7'}, {';', '8'}, {'\'', '9'},
+      {'p', '4'}, {'[', '5'}, {']', '6'}, {'0', '1'}, {'-', '2'}, {'=', '3'}};
+  const std::map<char, char> cursorKeyMap = {
+      {',', KEY_UP_ARROW},    {'.', KEY_DOWN_ARROW}, {'m', KEY_LEFT_ARROW},
+      {'/', KEY_RIGHT_ARROW}, {'k', KEY_HOME},       {'\\', KEY_END},
+      {'l', KEY_PAGE_UP},     {';', KEY_PAGE_DOWN}};
+  if (mode == KeyboardMode::normal) {
+    bleKey.write(k);
+  } else if (mode == KeyboardMode::numeric) {
+    if (numKeyMap.find(k) != numKeyMap.end()) {
+      bleKey.write(numKeyMap.at(k));
+    }
+  } else if (mode == KeyboardMode::cursor) {
+    if (cursorKeyMap.find(k) != numKeyMap.end()) {
+      bleKey.write(cursorKeyMap.at(k));
+    }
+  } else if (mode == KeyboardMode::media) {
+    switch (k) {
+      case '=':
+        bleKey.write(KEY_MEDIA_PLAY_PAUSE);
+        break;
+      case '0':
+        bleKey.write(KEY_MEDIA_PREVIOUS_TRACK);
+        break;
+      case '-':
+        bleKey.write(KEY_MEDIA_NEXT_TRACK);
+        break;
+      default:
+        break;
+    }
+  }
+}
 void keySend(m5::Keyboard_Class::KeysState key) {
+  static KeyboardMode keyboardMode = KeyboardMode::normal;
+  static uint8_t currentModifiers =
+      0;  // 直前に有効になっているモディファイアキー
+  if (key.fn) {
+    if (currentModifiers) {
+      M5Cardputer.Speaker.tone(1500, 100);
+      keyboardMode = KeyboardMode::normal;
+      currentModifiers = 0;
+      bleKey.releaseAll();
+    } else if (keyboardMode == KeyboardMode::normal) {
+      M5Cardputer.Speaker.tone(1600, 100);
+      keyboardMode = KeyboardMode::cursor;
+    } else if (keyboardMode == KeyboardMode::cursor) {
+      M5Cardputer.Speaker.tone(1700, 100);
+      keyboardMode = KeyboardMode::numeric;
+    } else if (keyboardMode == KeyboardMode::numeric) {
+      M5Cardputer.Speaker.tone(1800, 100);
+      keyboardMode = KeyboardMode::media;
+    } else if (keyboardMode == KeyboardMode::media) {
+      M5Cardputer.Speaker.tone(1500, 100);
+      keyboardMode = KeyboardMode::normal;
+    }
+  }
+  if (key.opt) {
+    M5Cardputer.Speaker.tone(1400, 100);
+    // bleKey.press(KEY_LEFT_ALT);
+  }
   if (key.modifiers) {
+    currentModifiers = key.modifiers;
     if (key.alt) {
-      Serial.println("alt");
+      M5Cardputer.Speaker.tone(1300, 100);
       bleKey.press(KEY_LEFT_ALT);
     }
     if (key.ctrl) {
-      Serial.println("ctrl");
+      M5Cardputer.Speaker.tone(1200, 100);
       bleKey.press(KEY_LEFT_CTRL);
     }
     if (key.shift) {
-      Serial.println("shift");
-    }
-    if (key.fn) {
-      Serial.println("fn");
-    }
-    if (key.opt) {
-      Serial.println("opt");
+      M5Cardputer.Speaker.tone(1100, 100);
+      bleKey.press(KEY_LEFT_SHIFT);
     }
   } else {
     if (key.del) {
@@ -43,14 +105,12 @@ void keySend(m5::Keyboard_Class::KeysState key) {
       bleKey.write(KEY_RETURN);
     }
     if (key.space) {
-      Serial.println("space");
     }
     if (key.tab) {
       bleKey.write(KEY_TAB);
     }
     for (auto i : key.word) {
-      M5Cardputer.Speaker.tone(4000, 5);
-      bleKey.write(i);
+      charSend(i, keyboardMode);
     }
     bleKey.releaseAll();
   }
