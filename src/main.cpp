74d5f3aadfbbb398cc1272442cc2054b0ad7f10a
KIRIAKE Masanori <seiken@rd01.net>
2025-05-06 12:37:42 +0900

    Added a mode to send screen reader commands.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,43 @@
 #include <map>
 
 BleKeyboard bleKey;
-const unsigned int keyInputTimeout = 180000;  // 3分
 
+// ３分間キー入力がなければスリープ
+const unsigned int keyInputTimeout = 180000;
 // 最後のキー入力時間
 unsigned long lastKeyInput = 0;
+
+enum class KeyboardMode {
+  normal,
+  numeric,
+  cursor,
+  media,
+  voice_over,
+  talkback,
+  narrator,
+  nvda,
+  jaws
+};
+
+std::map<KeyboardMode, std::array<uint8_t, 2>> srCommandKeyMap = {
+    {KeyboardMode::voice_over, {KEY_LEFT_CTRL, KEY_LEFT_ALT}},
+    {KeyboardMode::talkback, {KEY_LEFT_ALT, 0}},
+    {KeyboardMode::nvda, {KEY_NUM_0, 0}},
+    {KeyboardMode::jaws, {KEY_NUM_0, 0}},
+};
+
+const std::map<KeyboardMode, float> modeToneMap = {
+    {KeyboardMode::normal, 1000},     {KeyboardMode::cursor, 1100},
+    {KeyboardMode::numeric, 1200},    {KeyboardMode::media, 1300},
+    {KeyboardMode::voice_over, 1400}, {KeyboardMode::talkback, 1500},
+    {KeyboardMode::narrator, 1600},   {KeyboardMode::jaws, 1700},
+    {KeyboardMode::nvda, 1800}};
+
+constexpr float toneCtrlKey = 2000;
+constexpr float toneAltKey = 2100;
+constexpr float toneShiftKey = 2200;
+constexpr float toneOptKey = 2300;
+constexpr float toneFnKey = 2400;
 
 void checkBattery() {
   if (millis() > keyInputTimeout + lastKeyInput) {
@@ -31,9 +64,8 @@ void notifyConnection() {
     M5Cardputer.Speaker.tone(2000, 100);
   }
 }
-enum class KeyboardMode { normal, numeric, cursor, function, media };
 
-void charSend(char k, KeyboardMode mode) {
+void charSend(char k, KeyboardMode keyMode, KeyboardMode srMode) {
   const std::map<char, char> numKeyMap = {
       {',', '*'}, {'.', '0'}, {'/', '#'}, {'l', '7'}, {';', '8'}, {'\'', '9'},
       {'p', '4'}, {'[', '5'}, {']', '6'}, {'0', '1'}, {'-', '2'}, {'=', '3'}};
@@ -41,17 +73,23 @@ void charSend(char k, KeyboardMode mode) {
       {',', KEY_UP_ARROW},    {'.', KEY_DOWN_ARROW}, {'m', KEY_LEFT_ARROW},
       {'/', KEY_RIGHT_ARROW}, {'k', KEY_HOME},       {'\\', KEY_END},
       {'l', KEY_PAGE_UP},     {';', KEY_PAGE_DOWN}};
-  if (mode == KeyboardMode::normal) {
+  // 必要があればスクリーンリーダーコマンドキーを送信
+  if (srMode != KeyboardMode::normal) {
+    for (auto i : srCommandKeyMap.at(srMode)) {
+      if (i) bleKey.press(i);
+    }
+  }
+  if (keyMode == KeyboardMode::normal) {
     bleKey.write(k);
-  } else if (mode == KeyboardMode::numeric) {
+  } else if (keyMode == KeyboardMode::numeric) {
     if (numKeyMap.find(k) != numKeyMap.end()) {
       bleKey.write(numKeyMap.at(k));
     }
-  } else if (mode == KeyboardMode::cursor) {
+  } else if (keyMode == KeyboardMode::cursor) {
     if (cursorKeyMap.find(k) != cursorKeyMap.end()) {
       bleKey.write(cursorKeyMap.at(k));
     }
-  } else if (mode == KeyboardMode::media) {
+  } else if (keyMode == KeyboardMode::media) {
     switch (k) {
       case '=':
         bleKey.write(KEY_MEDIA_PLAY_PAUSE);
@@ -66,54 +104,95 @@ void charSend(char k, KeyboardMode mode) {
         break;
     }
   }
+  if (srMode != KeyboardMode::normal) {
+    bleKey.releaseAll();
+  }
 }
+
 void keySend(m5::Keyboard_Class::KeysState key) {
   // 現在のキーボードモード
   static KeyboardMode keyboardMode = KeyboardMode::normal;
+  static KeyboardMode srMode = KeyboardMode::voice_over;
   // 直前に有効になっているモディファイアキー
   static uint8_t currentModifiers = 0;
 
   // タイムアウト内にキー入力したよ。
   lastKeyInput = millis();
   if (key.fn) {
+    // キーボードモード切替
     if (currentModifiers) {
-      M5Cardputer.Speaker.tone(1500, 100);
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::normal), 100);
       keyboardMode = KeyboardMode::normal;
       currentModifiers = 0;
       bleKey.releaseAll();
     } else if (keyboardMode == KeyboardMode::normal) {
-      M5Cardputer.Speaker.tone(1600, 100);
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::cursor), 100);
       keyboardMode = KeyboardMode::cursor;
     } else if (keyboardMode == KeyboardMode::cursor) {
-      M5Cardputer.Speaker.tone(1700, 100);
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::numeric), 100);
       keyboardMode = KeyboardMode::numeric;
     } else if (keyboardMode == KeyboardMode::numeric) {
-      M5Cardputer.Speaker.tone(1800, 100);
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::media), 100);
       keyboardMode = KeyboardMode::media;
     } else if (keyboardMode == KeyboardMode::media) {
-      M5Cardputer.Speaker.tone(1500, 100);
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::normal), 100);
+      keyboardMode = KeyboardMode::normal;
+    } else {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::normal), 100);
       keyboardMode = KeyboardMode::normal;
     }
-  }
-  if (key.opt) {
-    M5Cardputer.Speaker.tone(1400, 100);
-    // bleKey.press(KEY_LEFT_ALT);
-  }
-  if (key.modifiers) {
+  } else if (key.opt) {
+    // スクリーンリーダー制御モード
+    if (currentModifiers) {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::normal), 100);
+      srMode = KeyboardMode::normal;
+      currentModifiers = 0;
+      bleKey.releaseAll();
+    } else if (srMode == KeyboardMode::normal) {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::voice_over), 100);
+      srMode = KeyboardMode::voice_over;
+    } else if (srMode == KeyboardMode::voice_over) {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::talkback), 100);
+      srMode = KeyboardMode::talkback;
+    } else if (srMode == KeyboardMode::talkback) {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::jaws), 100);
+      srMode = KeyboardMode::jaws;
+    } else if (srMode == KeyboardMode::jaws) {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::nvda), 100);
+      srMode = KeyboardMode::nvda;
+    } else if (srMode == KeyboardMode::nvda) {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::normal), 100);
+      srMode = KeyboardMode::normal;
+    } else {
+      M5Cardputer.Speaker.tone(modeToneMap.at(KeyboardMode::voice_over), 100);
+      srMode = KeyboardMode::normal;
+    }
+  } else if (key.modifiers) {
+    // 修飾キー
     currentModifiers = key.modifiers;
     if (key.alt) {
-      M5Cardputer.Speaker.tone(1300, 100);
-      bleKey.press(KEY_LEFT_ALT);
-    }
-    if (key.ctrl) {
-      M5Cardputer.Speaker.tone(1200, 100);
-      bleKey.press(KEY_LEFT_CTRL);
-    }
-    if (key.shift) {
-      M5Cardputer.Speaker.tone(1100, 100);
-      bleKey.press(KEY_LEFT_SHIFT);
+      M5Cardputer.Speaker.tone(toneAltKey, 100);
+    } else if (key.ctrl) {
+      M5Cardputer.Speaker.tone(toneCtrlKey, 100);
+    } else if (key.shift) {
+      M5Cardputer.Speaker.tone(toneShiftKey, 100);
     }
   } else {
+    if (currentModifiers) {
+      if (currentModifiers & 0b0001) {
+        Serial.println("Ctrl");
+        bleKey.press(KEY_LEFT_CTRL);
+      }
+      if (currentModifiers & 0b0010) {
+        bleKey.press(KEY_LEFT_SHIFT);
+        Serial.println("Shift");
+      }
+      if (currentModifiers & 0b0100) {
+        bleKey.press(KEY_LEFT_ALT);
+        Serial.println("Alt");
+      }
+    }
+    // 普通の文字
     if (key.del) {
       bleKey.write(KEY_BACKSPACE);
       M5Cardputer.Speaker.tone(3000, 5);
@@ -127,10 +206,13 @@ void keySend(m5::Keyboard_Class::KeysState key) {
       bleKey.write(KEY_TAB);
     }
     for (auto i : key.word) {
-      charSend(i, keyboardMode);
+      charSend(i, keyboardMode, srMode);
       M5Cardputer.Speaker.tone(4000, 5);
     }
-    bleKey.releaseAll();
+    if (currentModifiers) {
+      bleKey.releaseAll();
+      currentModifiers = 0;
+    }
   }
 }
 
